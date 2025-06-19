c79f5411ff2cf564b70d0072cb10526d573d17a7
Marco Lopes <marco.lopes.araujo@gmail.com>
2025-06-19 12:58:23 -0300

    feat: init project

---
+++ src/main/java/com/skillrowz/elojob/ElojobApplication.java
@@ -0,0 +1,13 @@
+package com.skillrowz.elojob;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElojobApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElojobApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/skillrowz/elojob/ElojobApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skillrowz.elojob;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElojobApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
